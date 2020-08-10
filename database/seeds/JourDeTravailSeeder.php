96da48797190824dc3be1754d7c6802068d7b15f
Zakaria Sadek <zakariasadek20@gmail.com>
2020-08-10 22:43:12 +0100

    fix err (add each doctor sum element from relations)

--- database/seeds/JourDeTravailSeeder.php
+++ database/seeds/JourDeTravailSeeder.php
@@ -11,18 +11,24 @@ class JourDeTravailSeeder extends Seeder
      */
     public function run()
     {
-        $NbrJourDeTravail =(int) $this->command->ask('How many of Jour De Travail you want generate ? [default 500]', 500);
+        // $NbrJourDeTravail =(int) $this->command->ask('How many of Jour De Travail you want generate ? [default 500]', 500);
 
         $docteurs = App\Docteur::all();
         if ($docteurs->count() == 0) {
             $this->command->info('please create some docteurs');
             return;
         }
-        $NbrJourDeTravail = $NbrJourDeTravail < $docteurs->count() ? $NbrJourDeTravail * $docteurs->count() : $NbrJourDeTravail;
+        // $NbrJourDeTravail = $NbrJourDeTravail < $docteurs->count() ? $NbrJourDeTravail * $docteurs->count() : $NbrJourDeTravail;
 
-        factory(App\JourDeTravail::class, $NbrJourDeTravail)->make()->each(function ($jourDeTravail) use ($docteurs) {
-            $jourDeTravail->docteur_id = $docteurs->random()->id;
-            $jourDeTravail->save();
+        // factory(App\JourDeTravail::class, $NbrJourDeTravail)->make()->each(function ($jourDeTravail) use ($docteurs) {
+        //     $jourDeTravail->docteur_id = $docteurs->random()->id;
+        //     $jourDeTravail->save();
+        // });
+        $docteurs->each(function($docteur){
+            factory(App\JourDeTravail::class, 7)->make()->each(function ($jourDeTravail) use ($docteur) {
+                $jourDeTravail->docteur_id = $docteur->id;
+                $jourDeTravail->save();
+            });
         });
     }
 }
