46714b56b67f3a2177ad47c6517ff0d54f41fa9f
Zakaria Sadek <zakariasadek20@gmail.com>
2020-08-11 16:21:29 +0100

    optimize code by deletion relationships call because we added them in property  in model Docteur,and fix errors in SearchByDistance(), and remove calcDistance()

--- app/Http/Controllers/DocteurController.php
+++ app/Http/Controllers/DocteurController.php
@@ -19,71 +19,34 @@ class DocteurController extends Controller
     public function index(Request $request)
     {
         $perPage = $request->input('per_page') ?? 5;
-        return DocteurResource::collection(Docteur::with(
-            ['specialites', 'ville', 'position', 'cabinet']
-        )->paginate($perPage)->appends([
+        return DocteurResource::collection(Docteur::paginate($perPage)->appends([
             'per_page' => $perPage
         ]));
-
-        // return $this->distance( 33.266158, -7.581950, 33.277113, -7.579224);
-
-
     }
+
     public function SearchByDistance(SearchDocteurDistanceRequest $request)
     {
         $lat = (float)$request->input('latitude');
         $long = (float)$request->input('longitude');
         $zone = (float)$request->input('zone');
-        $perPage = (float) $request->input('per_page') ?? 5.0;
+        // $perPage = (float) $request->input('per_page') ?? 5.0;
 
-        $doctors = DocteurResource::collection(Docteur::with(
-            ['specialites', 'ville', 'position', 'cabinet']
-        )
-            ->paginate($perPage)->appends([
-                'per_page' => $perPage
-            ]));
-        // $doctors->
-        foreach ($doctors as $doctor) {
+        $doctors = DocteurResource::collection(Docteur::all());
 
-            if ($this->calcDistance($lat, $long, (float)$doctor->position->latitude, (float)$doctor->position->longitude) > $zone) {
-                // $doctors->forget($doctors,$doctor);
-            }
-        }
+        $doctors = $doctors->filter(function ($doctor) use ($lat, $long, $zone) {
+            return $doctor->calcDistance($lat, $long) < $zone;
+        });
 
         return $doctors;
-
-        // return DocteurResource::collection(Docteur::with(
-        //     ['specialites', 'ville', 'position', 'cabinets']
-        // )->where()->get());
-    }
-    private function calcDistance($lat1, $long1, $lat2, $long2)
-    {
-        $degrees =
-
-            (sin(deg2rad($lat1)) * sin(deg2rad($lat2))) +
-            (cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
-                cos(deg2rad($long1 - $long2)));
-
-        $degrees = acos($degrees);
-        $degrees = rad2deg($degrees);
-        $distance = $degrees * 111.13384;
-        return  round($distance, 2);
     }
 
     public function SearchByVille(SearchByVilleRequest $request)
     {
         $ville_name = $request->input('ville_name');
-        // $ville=Ville::with(['docteurs','docteurs.specialites','docteurs.position',
-        //                     'docteurs.cabinet','docteurs.ville','docteurs.services'])
-        //         ->where('nom','like','%'.$ville_name.'%')->first();
-        // $docteurs=$ville->docteurs;
-        // return $docteurs;
-        // return DocteurResource::collection($docteurs);
-        return DocteurResource::collection(Docteur::with(
-            ['specialites', 'ville', 'position', 'cabinet','services']
-        )->whereHas('ville', function ($query) use ($ville_name) {
+
+        return DocteurResource::collection(Docteur::whereHas('ville', function ($query) use ($ville_name) {
             $query->where('nom', 'like', '%' . $ville_name . '%');
-        })->get());
+            })->get());
     }
 
     /**
